948e7e00dc40353b2fedcb61c136d697d37ed4aa
苏振鹏-金现代 <su__zhenpeng@163.com>
2017-12-01 15:29:10 +0800

    KL-4 人员字段精简

--- kl-main/src/main/java/ax/kl/mapper/SysBusinessUserMapper.java
+++ kl-main/src/main/java/ax/kl/mapper/SysBusinessUserMapper.java
@@ -12,20 +12,30 @@ import java.util.List;
 @Repository
 public interface SysBusinessUserMapper {
 
-    //获取岗位类型
+    /**
+     * 获取岗位类型
+     */
     List<WorkTypeInfo> getWorkTypeInfo();
 
 
-    //根据点击的节点及其子节点的ID值来对人员进行条件查询
+    /**
+     * 根据点击的节点及其子节点的ID值来对人员进行条件查询
+     */
     List<SysBusinessUser> getBusinessUserList(Page page, @Param("typeCode") String typeCode, @Param("searchName") String searchName);
 
-    //添加数据
+    /**
+     * 添加数据
+     */
     void insertBusinessUser(SysBusinessUser businessUser);
 
-    //更新数据
+    /**
+     * 更新数据
+     */
     void updateBusinessUser(SysBusinessUser businessUser);
 
-    //删除数据
+    /**
+     * 删除数据
+     */
     void deleteBusinessUser(String[] idLists);
 
     /**
