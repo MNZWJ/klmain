6340d8f1196e3c98f6548cbdd200a24a0b1f8a71
苏振鹏-金现代 <su__zhenpeng@163.com>
2017-12-05 18:08:55 +0800

    KL-14 化工企业分布图完善查询及展示字段

--- kl-main/src/main/resources/static/ProjectScripts/Common/common.js
+++ kl-main/src/main/resources/static/ProjectScripts/Common/common.js
@@ -2,3 +2,5 @@
 var ScaleCodeDictId='c101f4c7-f8cd-4daa-a9fd-a274c69277a5';
 //企业行业
 var IndustryCodeDictId='29485cb6-bf2e-4892-ab3a-083ffbb2e6f9';
+//企业类型
+var TypeCodeDictId='22bd341b-5ea1-4364-a4cf-8496f3132492';
