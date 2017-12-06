7055f56815611e355b2555fabeea4737b296c9ee
xum <15865264791@163.com>
2017-12-06 14:16:46 +0800

    KL-16 session超时

---
+++ kl-main/src/main/java/ax/kl/common/Auth.java
@@ -0,0 +1,9 @@
+package ax.kl.common;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Auth {
+}
