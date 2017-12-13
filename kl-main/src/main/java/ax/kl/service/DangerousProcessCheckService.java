6b5cf82ef62f7652347233191d5eb1234dcbe897
马小龙-金现代 <misak_mxl@163.com>
2017-12-13 13:35:37 +0800

    导出Excel

--- kl-main/src/main/java/ax/kl/service/DangerousProcessCheckService.java
+++ kl-main/src/main/java/ax/kl/service/DangerousProcessCheckService.java
@@ -4,7 +4,10 @@ import ax.kl.entity.CompanyInfo;
 import com.baomidou.mybatisplus.plugins.Page;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 /**
+ * 危险化工工艺
  * @author Created by mxl
  * @version 创建时间：${date} ${time}
  */
@@ -16,4 +19,20 @@ public interface DangerousProcessCheckService {
      * @return
      */
     Page<CompanyInfo> getProcessList(Page page, @Param("companyName") String companyName, @Param("risk") String risk);
+    /**
+     * 获取待导出的危险化工工艺总数
+     * @param companyName
+     * @param risk
+     * @return
+     */
+    int getExportMajorCount(String companyName, String risk);
+
+    /**
+     * 获取待导出的危险化工工艺列表
+     * @param companyName
+     * @param risk
+     * @return
+     */
+    List<CompanyInfo> getExportMajor(int pageIndex, int pageSize, String companyName, String risk);
 }
+
