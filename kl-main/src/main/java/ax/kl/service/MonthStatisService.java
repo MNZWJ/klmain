3a13f0305b932f86fbeb8fca966dcacb728e31a2
xum <15865264791@163.com>
2017-12-27 19:31:15 +0800

    KL-66 定时统计月度数据

---
+++ kl-main/src/main/java/ax/kl/service/MonthStatisService.java
@@ -0,0 +1,14 @@
+package ax.kl.service;
+
+
+/**
+ * @author Created by xum
+ * @version 创建时间：${date} ${time}
+ */
+public interface MonthStatisService {
+
+    /**
+     * 统计月度数据
+     */
+    void statisMonthData();
+}
